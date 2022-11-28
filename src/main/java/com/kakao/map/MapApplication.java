7e5f9bfb69f2db187d9945dc8df7619297f160bf
dozero25 <ehdud05264@naver.com>
2022-11-28 20:07:13 +0900

    11/28 코리아IT spring io 설정

---
+++ src/main/java/com/kakao/map/MapApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kakao/map/MapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
